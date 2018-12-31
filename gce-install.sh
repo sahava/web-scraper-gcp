5fa8edfc908320e98478cf6a57abf5cc383060a8
Simo Ahava <simo.s.ahava@gmail.com>
2018-12-31 10:24:22 +0200

    Update GCE install sh

--- gce-install.sh
+++ gce-install.sh
@@ -12,10 +12,6 @@ sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
 
 apt-get update && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont --no-install-recommends
 
-rm -rf /var/lib/apt/lists/*
-apt-get purge --auto-remove -y curl
-rm -rf /src/*.deb
-
 mkdir /opt/nodejs
 curl https://nodejs.org/dist/latest/node-v11.6.0-linux-x64.tar.gz | tar vxzf - -C /opt/nodejs --strip-components=1
 ln -s /opt/nodejs/bin/node /usr/bin/node
@@ -24,7 +20,7 @@ ln -s /opt/nodejs/bin/npm /usr/bin/npm
 git clone https://github.com/sahava/web-scraper-gcp.git
 
 cd web-scraper-gcp
-npm install
+sudo npm install
 gsutil cp ${bucket} .
 node index.js
 
