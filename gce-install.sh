dfc34c200ef7784f3761c43378977b6bf6f75481
Simo Ahava <simo.s.ahava@gmail.com>
2020-06-09 17:11:06 +0300

    Update install script

--- gce-install.sh
+++ gce-install.sh
@@ -1,17 +1,15 @@
-#!/usr/bin/env bash                                                                                                                                                                                                                             
+#!/usr/bin/env bash
 
 bucket='gs://web-scraper-config/config.json'
 
 set -v
 
+apt-get update
+apt-get install -y chromium
+apt-get install -y libgbm-dev
+
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
-apt-get update && apt-get install -yq git libgconf-2-4 nodejs
-apt-get update && apt-get install -y wget --no-install-recommends
-
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
-apt-get update && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont --no-install-recommends
+apt-get install -yq git libgconf-2-4 nodejs
 
 git clone https://github.com/sahava/web-scraper-gcp.git
 
